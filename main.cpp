7262cecca7b7f0860c2d92d38f7bc0a41da431b4
Harshith.mardithaya <88697745+notBeastKing@users.noreply.github.com>
2022-10-10 23:42:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 #include<iostream>
 #include<cmath>
 
-#define G 6.67 * pow(10,-2)
+#define G 6.67 * pow(10,-3)
+#define MAX(x,y) x>=y?x:y //
+                         //
+#define MAGN(v) sqrt(pow(v.x,2) + pow(v.y,2))
+
+#define NORM(v) v/(float)MAGN(v)
+
 
 using namespace std;
 using namespace sf;
+
 
 class logic{
 
@@ -14,45 +21,53 @@ public:
     float distance;
     float forc;
     float acceleration;
-    float velocity;
+     float velocity;
+    Vector2f accelerating;
+    Vector2f Velocityv;
+    Vector2f  forcv;
     Vector2f directionm;
-
 
     logic()
     {
         body.setFillColor(Color(255, 95, 95)); // red
+        Velocityv = Vector2f (0,0);
     }
 
     logic(string color)
     {
         body.setFillColor(Color( 95, 255, 250)); // blue
+        Velocityv = Vector2f (0,0);
     }
 
 
     void create_ball(float x, float y)
     {
+
         body.setRadius(50.f);
         Vector2f position(x,y);
         body.setPosition(position);
     }
 
-    Vector2f direction(Vector2f p1, Vector2f p2)
+    Vector2f force(float mass1, float mass2)
     {
-        distance = sqrt((pow((p2.x - p1.x),2) )+ pow((p2.y - p1.y),2));
-        directionm = (p2 - p1)/distance;
-        return directionm;
+//        cout<<MAX(distance,90)<<"\n";
+        forc = (G * mass1 * mass2)/(pow(MAX(distance,90),2));
+          forcv  = forc * directionm;
+
+        cout<<"force : " << forc<<"\n";
+         accelerating = forcv/mass1;
+         Velocityv = Velocityv + accelerating;
+        return Velocityv;
     }
 
-    float force(float mass1, float mass2)
+    Vector2f direction(Vector2f p1, Vector2f p2)
     {
-        if(distance < 100)
-        {
-            distance = 10;
-        }
-        forc = (G * mass1 * mass2)/(pow(distance,2));
-        acceleration = forc/mass1;
-        velocity += acceleration;
-        return velocity;
+
+        directionm = (p2 - p1);
+        distance = MAGN(directionm);
+        directionm = NORM(directionm);
+
+        return directionm;
     }
 
 };
@@ -68,15 +83,16 @@ int main(int argc, char *argv[])
     logic b1;
     logic b2(cyan);
 
-    b1.create_ball(1500/2, 600/2);
-    float massb1 = 10;
+    b1.create_ball(0, 900/2);
+    float massb1 = 100000;
 
-    b2.create_ball(0,0);
-    float massb2 = 10;
+
+    b2.create_ball(1500,900/2);
+    float massb2 = 100000;
 
     Event event;
     Clock clock;
-    window.setFramerateLimit(100000);
+    window.setFramerateLimit(60);
     while(window.isOpen())
     {
         Time dt = clock.restart();
@@ -88,16 +104,14 @@ int main(int argc, char *argv[])
             }
         }
 
-        Vector2f directb1 = b1.direction(b1.body.getPosition(), b2.body.getPosition());
-        Vector2f directb2 = b2.direction(b2.body.getPosition(), b1.body.getPosition());
+        Vector2f directb1 = b1.direction(b1.body.getPosition(), b2.body.getPosition()); //direction of b1
+        Vector2f directb2 = b2.direction(b2.body.getPosition(), b1.body.getPosition()); // direction of b2
 
-       float magnib1 = b1.force(massb1, massb2);
-       float magnib2 = b2.force(massb2, massb1);
+       Vector2f magnib1 = b1.force(massb1, massb2); // velocity for b1
+       Vector2f magnib2 = b2.force(massb2, massb1); // velocity for b2
 
-       b1.body.move(directb1 * (magnib1 * 100.f));
-       b2.body.move(directb2 * (magnib2 * 100.f));
-
-       cout<<b1.body.getPosition().x<<" "<<b1.body.getPosition().y<<"\n";
+           b2.body.setPosition(b2.body.getPosition()  + magnib2 );
+           b1.body.setPosition(b1.body.getPosition()  + magnib1 );
 
         window.clear();
         window.draw(b1.body);
